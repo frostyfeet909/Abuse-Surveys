3e521a5847494e03ba7ec710a431f4b9fea70796
Algernon Sampson <algiesampson@gmail.com>
2021-05-05 22:15:28 +0100

    Update basic_test.py

--- Tests/basic_test.py
+++ Tests/basic_test.py
@@ -7,7 +7,7 @@ def run():
     path.insert(1, './')
     import Abuse_Survey
     Abuse_Survey.abuse_survey_monkey(
-        2, 2, 'XQ6J57B', protections=True, verbosity=2)
+        5, 5, 'XQ6J57B', protections=True, verbosity=2)
 
 
 if __name__ == '__main__':
